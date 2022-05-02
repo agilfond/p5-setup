8d0bf804133021a2956cec217634f16b78a87ce1
agilfond <92835815+agilfond@users.noreply.github.com>
2022-05-02 08:42:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  background(0);
+
 }
 
 function draw() {
-  background(0);
-  color(255);
+  fill('white');
   ellipse(20,100,100);
+  fill ('pink');
   ellipse(20,300,300);
 }
